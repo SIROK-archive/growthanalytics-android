a77e307063aed33dfeb9764ccd991bb17cd8cb15
Shigeru Ogawa <ogawa@sirok.co.jp>
2014-11-19 16:18:15 +0900

    create TrackEventOption

---
+++ source/src/com/growthbeat/analytics/options/TrackEventOption.java
@@ -0,0 +1,10 @@
+package com.growthbeat.analytics.options;
+
+public class TrackEventOption {
+
+	public static final int DEFAULT = 0x00;
+	public static final int FIRST_TIME = 0x01;
+	public static final int ONCE = 0x02;
+	public static final int FIREST_TIME_ONCE = FIRST_TIME | ONCE;
+
+}
