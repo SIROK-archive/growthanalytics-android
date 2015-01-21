9ed60603eaeba032e632b6f38445dd2e6a7e3bad
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-01-21 14:12:55 +0900

    Remove unnecessary code in MainActivity

--- sample/src/com/growthbeat/analytics/MainActivity.java
+++ sample/src/com/growthbeat/analytics/MainActivity.java
@@ -1,13 +1,11 @@
 package com.growthbeat.analytics;
 
-import java.io.IOException;
 import java.util.UUID;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,8 +14,6 @@ import android.widget.Button;
 
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 import com.google.android.gms.ads.identifier.AdvertisingIdClient.Info;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -67,33 +63,14 @@ public class MainActivity extends ActionBarActivity {
 	private void sendAdvertisingId() {
 
 		new Thread(new Runnable() {
-
 			@Override
 			public void run() {
-
-				int count = 10;
-				Info adInfo = null;
 				try {
-					adInfo = AdvertisingIdClient.getAdvertisingIdInfo(MainActivity.this.getApplicationContext());
-					while (adInfo.getId() == null) {
-						if (adInfo.getId() != null)
-							break;
-						if (count < 0)
-							return;
-						try {
-							Thread.sleep(100);
-						} catch (InterruptedException e) {
-						}
-						count--;
-					}
-
-					if (adInfo.getId() != null && !adInfo.isLimitAdTrackingEnabled())
-						GrowthAnalytics.getInstance().setAdvertisingId(adInfo.getId());
-
-				} catch (IllegalStateException e) {
-				} catch (GooglePlayServicesRepairableException e) {
-				} catch (IOException e) {
-				} catch (GooglePlayServicesNotAvailableException e) {
+					Info adInfo = AdvertisingIdClient.getAdvertisingIdInfo(MainActivity.this.getApplicationContext());
+					if (adInfo.getId() == null || !adInfo.isLimitAdTrackingEnabled())
+						return;
+					GrowthAnalytics.getInstance().setAdvertisingId(adInfo.getId());
+				} catch (Exception e) {
 				}
 			}
 		}).start();
@@ -102,32 +79,17 @@ public class MainActivity extends ActionBarActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		switch (keyCode) {
-		case KeyEvent.KEYCODE_BACK:
-			GrowthAnalytics.getInstance().close();
-			finish();
-			return true;
-		}
-		return false;
 	}
 
 }
